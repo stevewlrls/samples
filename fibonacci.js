5149733e3e14f3239346bc1f3189557810361924
Steven Wheeler <stevewlrls@gmail.com>
2024-03-06 07:06:03 +0000

    Update examples

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,19 @@
+/**
+ * @module fibonacci.js
+ *
+ * Contains sample code to calculate an arbitrary Fibonacci Number,
+ *    F(n) = F(n-1) + F(n-2)
+ * where n is a positive integer and F(0) = F(1) = 1.
+ *
+ * Uses recursion, along with a 'memory' array (m) to store intermediate
+ * results so that they don't need to be recalculated, once found.
+ */
+
 const m = new Array()
 function F(n) {
+  if (n < 2) return 1
   if (m[n]) return m[n]
-  return (m[n] = n > 1 ? F(n - 1) + F(n - 2) : 1)
+  return F(n - 1) + F(n - 2)
 }
 
 console.log(F(10))
