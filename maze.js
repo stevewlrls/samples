47587ae00a9f9fc91274d8986b43313b536554eb
Steven Wheeler <stevewlrls@gmail.com>
2024-03-09 15:00:58 +0000

    Add maze game starter code.

---
+++ maze.js
@@ -0,0 +1,69 @@
+/**
+ * @module maze.js
+ *
+ * Provides the interaction logic to move a rabbit through a maze, to reach
+ * the 'goal' at the centre.
+ */
+
+const layout = [
+  '+--------+-----+---+',
+  '|        |     |   |',
+  '| +----+ +---- | | |',
+  '  |    | |     | | |',
+  '| | +--+ | +---+-| |',
+  '| | |    | |     | |',
+  '| | | +--+ | +-+ | |',
+  '| | |      | | | | |',
+  '| | | +----+ | | | |',
+  '| | | |      | | | |',
+  '| | | | ---+ | | | |',
+  '| | | |   x| | | | |',
+  '| | | +--+-+   | | |',
+  '| | |    | +---+ | |',
+  '| | +--+ |       | |',
+  '| |    | +---- --+ |',
+  '| +--+ |         | |',
+  '|    | +-- ------+ |',
+  '+--+ |             |',
+  '++ | +-------------|',
+  '|  | |             |',
+  '| ++ +------------ |',
+  '|                  |',
+  '+------------------+'
+]
+
+const hedge = 'images/thujenhecke-1487722_640.jpg'
+const path = 'images/imageBlank.png'
+const goal = 'images/carrot-6300849_640.png'
+
+const maze = document.getElementById('maze')
+const rabbit = document.getElementById('rabbit')
+
+let row, col
+
+function moveRabbit(r, c) {
+  rabbit.style.top = r * 40 + 'px'
+  rabbit.style.left = c * 40 + 'px'
+  row = r
+  col = c
+  if (mazeAt(r, c) === 'x') alert('Yay! Reached the goal!')
+}
+
+function mazeAt(r, c) {
+  return layout[r][c]
+}
+
+// Set up maze:
+for (const row of layout) {
+  for (const c of Array.from(row)) {
+    let el = new Image(40, 40)
+    el.src = c === 'x' ? goal : c === ' ' ? path : hedge
+    maze.appendChild(el)
+  }
+}
+
+moveRabbit(3, 0)
+
+// Put your code here. You need to catch the button click events and then test
+// whether the rabbit is able to move in that direction, before updating its
+// position.
